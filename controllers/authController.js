e48da46f1ff3bd67977b4a7df85d823eed2faed5
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-20 18:44:55 +0700

    refactor: auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const AppError = require('./../utils/appError');
 
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
@@ -41,23 +43,63 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
   // 1. getting token and check of it's there
   let token;
-
   if (
     req.headers.authorization &&
-    req.headers.authorization.startWith('Bearer')
+    req.headers.authorization.startsWith('Bearer')
   ) {
-    token = req.headers.authorization.split('')[1];
+    token = req.headers.authorization.split(' ')[1];
   }
-  // 2. verification token
 
   if (!token) {
     return next(
-      new AppError('You are not logged in! Please log in to get access', 401)
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2. verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
+
+  // 3. check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this user does no longer exist', 401)
     );
   }
 
-  // 3. check if user still exists
-
   // 4. check if usere changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again')
+    );
+  }
+
+  req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
+
+exports.forgetPassword = catchAsync(async (req, res, next) => {
+  // 1. get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address', 404));
+  }
+  // 2. generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. send it to users email
+});
+exports.resetPassword = (req, res, next) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
-router
-  .post('/signup', authController.signup)
-  .post('/login', authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgetPassword', authController.forgetPassword);
+router.post('/resetPassword', authController.resetPassword);
 
 router
   .route('/')
