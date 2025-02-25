eb598b92300095d9134afbbc0e4547ddc9f1a26f
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-25 18:36:20 +0700

    feat: add model reviews

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const helmet = require('helmet');
 
 const app = express();
-app.use(helmet);
+app.use(helmet());
 
 // ====================== 1. middleware ======================
 console.log(process.env.NODE_ENV);
@@ -68,6 +70,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
