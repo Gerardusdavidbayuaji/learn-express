670f9ecc6c8e164c9677385f34cb8c04daed4c1b
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-03-04 13:11:23 +0700

    refactor: import dev

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 const fs = require('fs');
 
 dotenv.config({ path: './config.env' });
-const app = require('../../app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -19,10 +20,16 @@ mongoose
   .catch((err) => console.error('DB connection failed:', err));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully created');
   } catch (error) {
     console.log(error);
@@ -33,6 +40,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted');
   } catch (error) {
     console.log(error);
