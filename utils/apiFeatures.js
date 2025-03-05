6e7bfdba5efc80aac3225baf2ba7b9dd1dd07b38
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-03-05 09:48:40 +0700

    fix: api features + email

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -5,12 +5,10 @@ class APIFeatures {
   }
 
   filter() {
-    // filter
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // advenced filter
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
